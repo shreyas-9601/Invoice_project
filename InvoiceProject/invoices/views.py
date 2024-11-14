ace9f22a07e5974caaf6f7c840624b8b40a09d2a
shreyas-9601 <shreyas9601@gmail.com>
2024-11-14 15:09:22 +0530

    add all files

---
+++ InvoiceProject/invoices/views.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Invoice
+from .serializers import InvoiceSerializer
+
+class InvoiceView(APIView):
+    def post(self, request):
+        serializer = InvoiceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        try:
+            invoice = Invoice.objects.get(pk=pk)
+        except Invoice.DoesNotExist:
+            return Response({"error": "Invoice not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = InvoiceSerializer(invoice, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
